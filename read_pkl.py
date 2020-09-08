288f856f6b16f80b9ac06782857f89436a771ef1
Alex-Hill94 <32838443+Alex-Hill94@users.noreply.github.com>
2020-09-09 00:10:29 +0100

    Create read_pkl.py

---
+++ read_pkl.py
@@ -0,0 +1,14 @@
+import numpy as np
+import illustris_python as il
+import pickle as pkl
+import pandas as pd
+
+sim = 'TNG300-1'
+dmo = False
+
+if dmo:
+    sim = sim+'-Dark'
+
+unpickled_df = pd.read_pickle("./"+sim+"_catalogue.pkl")
+
+StellarMass = unpickled_df["StellarMass"]
