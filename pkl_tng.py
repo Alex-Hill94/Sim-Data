c63ab63892b674717dc43c11404d3bc000ae0a8c
Alex-Hill94 <32838443+Alex-Hill94@users.noreply.github.com>
2020-09-09 00:25:27 +0100

    Update pkl_tng.py

--- pkl_tng.py
+++ pkl_tng.py
@@ -1,12 +1,13 @@
 import numpy as np
 import illustris_python as il
+import pickle as pkl
 import pandas as pd
 
 
 sim = 'TNG300-1'
 dmo = False
-physical = True
-hless = True
+#physical = True
+#hless = True
 
 
 if dmo:
@@ -14,7 +15,11 @@ if dmo:
 
 basePath = '/hpcdata0/simulations/IllustrisTNG/'+sim+'/output/'
 
-fields_sub = ['SubhaloFlag','SubhaloGrNr', 'SubhaloMass', 'SubhaloParent', 'SubhaloPos', 'SubhaloMassType']
+fields_sub = ['SubhaloGrNr', 'SubhaloMass', 'SubhaloParent', 'SubhaloPos']
+
+if dmo == False:
+    fields_sub = fields_sub + ['SubhaloFlag', 'SubhaloMassType']
+
 fields_grp = ['GroupFirstSub', 'GroupNsubs', 'Group_M_Crit200']
 
 subhalos = il.groupcat.loadSubhalos(basePath,99,fields=fields_sub)
@@ -34,19 +39,34 @@ GroupNumber = subhalos['SubhaloGrNr']
 COP = subhalos['SubhaloPos'] * a * h**-1/1000.
 COP_x, COP_y, COP_z = COP[:,0], COP[:,1], COP[:,2]
 GalaxyID = np.arange(0, len(COP), 1)
-StellarMass = subhalos['SubhaloMassType'][:,4]*h**-1
 HaloMass = halos['Group_M_Crit200'][GroupNumber]*h**-1
-Flags = subhalos['SubhaloFlag']
+SubhaloMass = subhalos['SubhaloMass']*h**-1
 
-df = pd.DataFrame({"SubGroupNumber": SubGroupNumber,
+if dmo == False:
+    StellarMass = subhalos['SubhaloMassType'][:,4]*h**-1
+    Flags = subhalos['SubhaloFlag']
+
+    df = pd.DataFrame({"SubGroupNumber": SubGroupNumber,
+                        "GroupNumber": GroupNumber,
+                        "COP_x": COP_x, 
+                        "COP_y": COP_y, 
+                        "COP_z": COP_z,
+                        "GalaxyID": GalaxyID,
+                        "Flags": Flags,
+                        "StellarMass": StellarMass,
+                        "SubhaloMass": SubhaloMass,
+                        "HaloMass": HaloMass})
+
+elif dmo:
+    df = pd.DataFrame({"SubGroupNumber": SubGroupNumber,
                     "GroupNumber": GroupNumber,
                     "COP_x": COP_x, 
                     "COP_y": COP_y, 
                     "COP_z": COP_z,
                     "GalaxyID": GalaxyID,
-                    "Flags": Flags,
-                    "StellarMass": StellarMass,
+                    "SubhaloMass": SubhaloMass,
                     "HaloMass": HaloMass})
+
 
 df.to_pickle('./'+sim+'_catalogue.pkl')
 
